c2e48d902b603c28484c35a94fe89e250b94511a
Diego <alrp.diego@gmail.com>
2021-10-13 11:36:25 +0200

    Main y casos

---
+++ Dijkstra/Dijkstra/Dijkstra.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	// Para la entrada por fichero.
+#ifndef DOMJUDGE
+	std::ifstream in("casos.txt");
+	auto cinbuf = std::cin.rdbuf(in.rdbuf());
+#endif
+
+
+	unsigned int numCasos;
+	std::cin >> numCasos;
+	// Resolvemos
+	for (int i = 0; i < numCasos; ++i) {
+		resuelveCaso();
+	}
+
+
+#ifndef DOMJUDGE // para dejar todo como estaba al principio
+	std::cin.rdbuf(cinbuf);
+	system("PAUSE");
+#endif
+
+	return 0;
+}
